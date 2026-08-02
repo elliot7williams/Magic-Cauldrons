51fb65922f49ee76a0b4ae312b0e9521673e0619
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:10:36 -0400

    Add Magic Cauldrons/Magic Cauldrons/Magic_CauldronsApp.swift

---
+++ Magic Cauldrons/Magic Cauldrons/Magic_CauldronsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Magic_CauldronsApp.swift
+//  Magic Cauldrons
+//
+//  Created by Elliot Williams on 2025-06-24.
+//
+
+import SwiftUI
+
+@main
+struct Magic_CauldronsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MagicCauldronsGame()
+        }
+    }
+}
