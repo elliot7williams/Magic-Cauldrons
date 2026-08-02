1d7f00dca7103f6c9fb65f55595bbde60c21cbb4
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:10:28 -0400

    Add Magic Cauldrons/Magic CauldronsTests/Magic_CauldronsTests.swift

---
+++ Magic Cauldrons/Magic CauldronsTests/Magic_CauldronsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Magic_CauldronsTests.swift
+//  Magic CauldronsTests
+//
+//  Created by Elliot Williams on 2025-06-24.
+//
+
+import Testing
+@testable import Magic_Cauldrons
+
+struct Magic_CauldronsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
