590194a46a8e148549fa6b5e2b45023f1fdcdee2
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:10:29 -0400

    Add Magic Cauldrons/Magic CauldronsUITests/Magic_CauldronsUITestsLaunchTests.swift

---
+++ Magic Cauldrons/Magic CauldronsUITests/Magic_CauldronsUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Magic_CauldronsUITestsLaunchTests.swift
+//  Magic CauldronsUITests
+//
+//  Created by Elliot Williams on 2025-06-24.
+//
+
+import XCTest
+
+final class Magic_CauldronsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
